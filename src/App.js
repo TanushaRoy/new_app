03f4698e1ac609ac40d5f37fd927ca2475eb335a
TanushaRoy <roytanusha53@gmail.com>
2025-06-13 05:38:06 +0530

    changes time 5.40

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="app-container">
-      <h1>My Clean React App</h1>
+      <h1>My Clean React Appppp</h1>
       <p>This is a simple UI without the React logo.</p>
       <button onClick={() => alert('Button clicked!')}>Click Me</button>
     </div>
